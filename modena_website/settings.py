980bbc8cc0e9eb521bee7887adf5d45ed150edcc
DimaPr7 <dprokhach@gmail.com>
2025-09-29 01:44:38 +0300

    Add Render domain to ALLOWED_HOSTS

--- modena_website/settings.py
+++ modena_website/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = "django-insecure-38(avtvcj8x_3$zsow2_hfnm20&fwtd2p-%1n5ui#+&$g5(a0v
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['modena-u9vt.onrender.com', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
